7c9ef3a94c95a407385f502840797ace362b3807
mgarnier11 <mgarnier11@gmail.com>
2019-07-11 00:05:00 +0200

    dev en cours

--- front/src/modules/DayGraph.jsx
+++ front/src/modules/DayGraph.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import { VictoryChart, VictoryAxis, VictoryLine } from 'victory';
+import { VictoryChart, VictoryTheme, VictoryAxis, VictoryLine } from 'victory';
 
 const componentName = 'DayGraph';
-const size = { x: 5, y: 6 };
+const size = { x: 15, y: 6 };
+
+const timerIntervall = 1 * 1000;
+
+function rnd(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 class DayGraph extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      timeTicks: this.getTimeTicks(),
+      timeTicks: [],
       dataTicks: []
     };
+
+    this.divRef = React.createRef();
 
     this.timer = undefined;
 
@@ -19,44 +27,101 @@ class DayGraph extends Component {
   }
 
   updateTicks() {
-    this.setState({ timeTicks: this.getTimeTicks() });
-    //pop first value and add actual date every 5 min
+    let timeTicks = [...this.state.timeTicks];
+
+    timeTicks.pop();
+
+    timeTicks.unshift(new Date());
+
+    let dataTicks = [...this.state.dataTicks];
+
+    dataTicks.pop();
+
+    dataTicks.unshift(this.nextDataTick(dataTicks[0]));
+
+    this.setState({ timeTicks, dataTicks });
   }
 
   getTimeTicks() {
     let ticks = [];
 
     let d = new Date();
-    ticks.push(d);
+    ticks.unshift(d);
 
-    while (ticks.length < 288) {
-      d = new Date(Date.parse(d) - 5 * 60 * 1000);
-      ticks.push(d);
+    while (ticks.length < 11) {
+      d = new Date(Date.parse(d) - timerIntervall);
+      ticks.unshift(d);
     }
 
-    return ticks;
+    return ticks.reverse();
+  }
+
+  getDataTicks() {
+    let ticks = [10];
+
+    while (ticks.length < 11) {
+      ticks.unshift(this.nextDataTick(ticks[0]));
+    }
+
+    return ticks.reverse();
+  }
+
+  nextDataTick(lastTick) {
+    let ud = rnd(0, 1);
+
+    let nextTick = ud === 0 ? lastTick + rnd(0, 5) : lastTick - rnd(0, 5);
+    if (nextTick < 0) nextTick = 0;
+    return nextTick;
   }
 
   componentDidMount() {
-    this.timer = setInterval(this.updateTicks, 5 * 60 * 1000);
+    this.timer = setInterval(this.updateTicks, timerIntervall);
+    this.setState({
+      timeTicks: this.getTimeTicks(),
+      dataTicks: this.getDataTicks()
+    });
   }
 
   componentWillUnmount() {
     clearInterval(this.timer);
   }
   render() {
+    const { timeTicks, dataTicks } = this.state;
+
     return (
-      <div className="w-100 h-100" style={{ backgroundColor: 'white' }}>
-        <VictoryChart>
+      <div
+        className="w-100 h-100"
+        style={{ backgroundColor: 'white' }}
+        ref={this.divRef}
+      >
+        <VictoryChart
+          theme={VictoryTheme.material}
+          width={this.divRef.current ? this.divRef.current.offsetWidth : 400}
+        >
           <VictoryAxis
             // tickValues specifies both the number of ticks and where
             // they are placed on the axis
-            tickValues={[]}
-            tickFormat={['test', 'Quarter 2', 'Quarter 3', '']}
+            tickValues={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+            tickFormat={timeTicks
+              .slice()
+              .reverse()
+              .map(
+                date =>
+                  `${date
+                    .getHours()
+                    .toString()
+                    .padStart(2, '0')}:${date
+                    .getMinutes()
+                    .toString()
+                    .padStart(2, '0')}:${date
+                    .getSeconds()
+                    .toString()
+                    .padStart(2, '0')}`
+              )}
           />
           <VictoryAxis
             dependentAxis
-            domain={[0, 25]}
+            domain={[0]}
             // tickFormat specifies how ticks should be displayed
             tickFormat={x => `${x}W`}
           />
@@ -66,14 +131,12 @@ class DayGraph extends Component {
               parent: { border: '1px solid #ccc' }
             }}
             interpolation="natural"
-            data={[
-              { x: 0, y: 8 },
-              { x: 1, y: 2 },
-              { x: 2, y: 3 },
-              { x: 3, y: 5 },
-              { x: 4, y: 4 },
-              { x: 5, y: 7 }
-            ]}
+            data={dataTicks
+              .slice()
+              .reverse()
+              .map((tick, i) => {
+                return { x: i, y: tick };
+              })}
           />
         </VictoryChart>
       </div>
